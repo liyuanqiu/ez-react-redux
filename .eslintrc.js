2a948c1710c83348f6af7db8935305d7fcac3ab2
李元秋 <yuanqiu.li@wz-inc.com>
2020-03-30 13:58:42 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'prettier/@typescript-eslint',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
